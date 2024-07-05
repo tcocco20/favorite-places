9b0ab159250ceb84357a98f53578fd79b3527b78
tcocco20 <theo@coccoscoding.dev>
2024-07-05 16:01:16 -0700

    started styling add new place form and added image picker package

--- components/Places/PlaceForm.tsx
+++ components/Places/PlaceForm.tsx
@@ -1,11 +1,31 @@
-import React from "react";
-import { Text, View } from "tamagui";
+import { colors } from "@/constants/Colors";
+import { useState } from "react";
+import { Form, Input, Label, ScrollView, Text, View } from "tamagui";
 
 const PlaceForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState<string>();
+
+  const changeTitleHandler = (text: string) => {
+    setEnteredTitle(text);
+  };
+
+  const submitFormHandler = () => {
+    console.log("Form Submitted");
+  };
   return (
-    <View>
-      <Text>PlaceForm</Text>
-    </View>
+    <ScrollView flex={1} p={24}>
+      <Form onSubmit={submitFormHandler}>
+        <Label fontWeight={700} color={colors.primary500}>
+          Title
+        </Label>
+        <Input
+          fontSize={16}
+          backgroundColor={colors.primary100}
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+        />
+      </Form>
+    </ScrollView>
   );
 };
 
