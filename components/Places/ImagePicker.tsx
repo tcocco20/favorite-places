51ae83c711e2b03454f6b9b58d2a0f662ea87902
tcocco20 <theo@coccoscoding.dev>
2024-07-12 19:07:20 -0700

    styled image preview and take image button

--- components/Places/ImagePicker.tsx
+++ components/Places/ImagePicker.tsx
@@ -1,12 +1,17 @@
+import { colors } from "@/constants/Colors";
+import { Camera } from "@tamagui/lucide-icons";
 import {
+  ImagePickerAsset,
   launchCameraAsync,
   PermissionStatus,
   useCameraPermissions,
 } from "expo-image-picker";
+import { useState } from "react";
 import { Alert } from "react-native";
-import { Button, Text, View } from "tamagui";
+import { Button, Image, Text, View } from "tamagui";
 
 const ImagePicker = () => {
+  const [pickedImage, setPickedImage] = useState<string | null>(null);
   const [cameraPermissionsInformation, requestPermission] =
     useCameraPermissions();
 
@@ -42,12 +47,46 @@ const ImagePicker = () => {
       quality: 0.5,
     });
 
-    console.log(image);
+    if (image.canceled) {
+      return;
+    }
+
+    setPickedImage(image.assets[0].uri);
   };
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = (
+      <Image width={"100%"} height={"100%"} source={{ uri: pickedImage }} />
+    );
+  }
+
   return (
     <View>
-      <View></View>
-      <Button onPress={takeImageHandler}>Take Image</Button>
+      <View
+        height={200}
+        width={"100%"}
+        marginVertical={8}
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor={colors.primary100}
+        borderRadius={4}
+      >
+        {imagePreview}
+      </View>
+
+      <Button
+        variant="outlined"
+        borderColor={colors.primary500}
+        margin={4}
+        onPress={takeImageHandler}
+        color={colors.primary500}
+        icon={Camera}
+        scaleIcon={1.5}
+      >
+        Take Image
+      </Button>
     </View>
   );
 };
